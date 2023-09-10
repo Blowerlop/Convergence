cfacbc49b1c49e730b3a04a45d5280954872e4ea
Blowerlop <72097300+Blowerlop@users.noreply.github.com>
2023-09-10 17:17:42 +0200

    Add gRPC Server files (#26)
    * Cleaned up project
    
    * Imported Netcode for GameObjects
    
    * Imported Lobby
    
    * Added LogFileExporter to discord
    
    * Remove unused default packages
    
    * Added Network ConnectionManager & added Discord Log Sharing & moved assets to Plugins folder
    
    * Reimported correctly ParrelSync
    
    * Console can now execute command with default parameters
    
    * Updated console command parameters hint display
    
    * Added the base of the LoadingManager
    
    * Add LogDisplay for in game log
    
    * Move Unity Project root
    
    * Update CI-CD Project Path
    
    * Update CI-CD Project Path
    
    * Update CI-CD Project Path for build jobs
    
    * Add gRPC Server files
    
    ---------
    
    Co-authored-by: Blowerlop <Blowerlop@users.noreply.github.com>

---
+++ gRPC/Server/GrpcTest/MTime.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace Utils
+{
+    public static class MTime
+    {
+        //Everything in ms
+
+        const int fps = 20;
+        const int timePerFrame = 1000 / fps; //in ms
+
+        static int deltaTime;
+
+        static int timer = 0;
+
+        public static event Action onTickCall;
+
+        public static async Task CountTickRate()
+        {
+            Stopwatch watch = new Stopwatch();
+
+            while (true)
+            {
+                deltaTime = (int)watch.ElapsedMilliseconds;            
+                watch.Restart();
+
+                timer += deltaTime;
+
+                while(timer >= timePerFrame)
+                {
+                    timer -= timePerFrame;
+
+                    onTickCall?.Invoke();
+                }
+
+                while((int)watch.ElapsedMilliseconds < timePerFrame) { }
+            }
+        }
+    }
+}
