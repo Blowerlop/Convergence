8593536cfd49ed7823d47a55bb51d7e244fa23cb
Blowerlop <Blowerlop@users.noreply.github.com>
2024-01-05 16:31:14 +0100

    Add possibility to center camera on the player

--- Unity/Assets/_Project/Scripts/Player/CameraController.cs
+++ Unity/Assets/_Project/Scripts/Player/CameraController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Runtime.CompilerServices;
 using Project.Extensions;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -18,9 +20,12 @@ namespace Project
     {
         [Title("Settings")]
         [SerializeField] private float _speed = 40.0f;
+        [SerializeField] private bool _canUseKeyboard = true;
+        [SerializeField] private bool _canUseMouse = true;
         
         [Title("References")]
         [SerializeField] private Camera _playerCamera;
+        [SerializeField] private Transform _player;
 
         private Vector3 _forward;
         private Vector3 _right;
@@ -34,6 +39,10 @@ namespace Project
 
         private void Update()
         {
+#if UNITY_EDITOR
+            if (Application.isFocused == false) return;
+#endif
+            
             ComputeMovement();
         }
         
@@ -41,10 +50,11 @@ namespace Project
         private void ComputeMovement()
         {
             Vector2 movementInput = Vector2.zero;
-            ComputeMovementWithKeyboard(ref movementInput);
-            ComputeMovementWithMouse(ref movementInput);
-
-            if (movementInput == Vector2.zero) return;
+            
+            if (_canUseKeyboard) ComputeMovementWithKeyboard(ref movementInput);
+            if (_canUseMouse) ComputeMovementWithMouse(ref movementInput);
+            
+            if (HasRequestedAMovement(movementInput) == false) return;
             
             Vector3 rawVelocity = _right * movementInput.x + _forward * movementInput.y;
             Vector3 velocity = rawVelocity.normalized * (_speed * Time.deltaTime);
@@ -59,10 +69,6 @@ namespace Project
 
         private void ComputeMovementWithMouse(ref Vector2 movementInput)
         {
-            #if UNITY_EDITOR
-            if (Application.isFocused == false) return;
-            #endif
-            
             Vector2 mousePosition = Mouse.current.position.value;
 
             if (mousePosition.x <= ScreenBorder.left) movementInput.x = -1.0f;
@@ -70,6 +76,32 @@ namespace Project
             
             if (mousePosition.y >= ScreenBorder.top) movementInput.y = 1.0f;
             else if (mousePosition.y <= ScreenBorder.bottom) movementInput.y = -1.0f;
+        }
+        
+        private bool HasRequestedAMovement(Vector2 movementInput)
+        {
+            return movementInput != Vector2.zero;
+        }
+
+        /// <summary>
+        /// There is maybe a wayyyy more efficient way to do it but in works for the moment.
+        /// </summary>
+        [Button]
+        private void CenterCameraOnPlayer()
+        {
+            if (Physics.Raycast(_playerCamera.transform.position, _playerCamera.transform.forward, out RaycastHit hit))
+            {
+            }
+            else return;
+
+            var result = (_player.transform.position - hit.point).RemoveAxis(EAxis.Y);
+            _playerCamera.transform.position += result;
+        }
+
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawLine(_playerCamera.transform.position, _player.transform.position);
         }
     }
 }
