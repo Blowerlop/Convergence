73d91f864676f22ddb22c45b0e7c15432309cbc4
Hélias <helias.gamonet@laposte.net>
2024-02-10 13:27:43 +0100

    Debbugs

--- Unity/Assets/_Project/Scripts/Spells/SpellManager.cs
+++ Unity/Assets/_Project/Scripts/Spells/SpellManager.cs
@@ -71,6 +71,8 @@ namespace Project.Spells
                 case PlayerPlatform.Mobile:
                     var spellHash = user.GetMobileSpell(spellIndex);
                     
+                    Debug.Log($"Trying to get spell {spellIndex} for mobile {user.ClientId} : {spellHash}");
+                    
                     spell = SpellData.GetSpell(spellHash);
 
                     if (spell == null)
