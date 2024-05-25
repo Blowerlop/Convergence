6f8e5be81c0b69a973bc418a3d10ace88667fd5f
Bao Nguyen <qbnguyen@outlook.fr>
2024-05-26 01:42:57 +0200

    balancing patch

--- Unity/Assets/_Project/Scripts/Projectile/Projectile.cs
+++ Unity/Assets/_Project/Scripts/Projectile/Projectile.cs
@@ -24,8 +24,8 @@ namespace Project._Project.Scripts
             Vector3 direction = _target.position - transform.position;
             Vector3 directionNormalized = direction.normalized;
             
-            transform.position += directionNormalized * (Time.fixedDeltaTime * _speed);
-            if (direction.sqrMagnitude < SOProjectile.HIT_RANGE * SOProjectile.HIT_RANGE)
+            transform.position += directionNormalized * (Time.fixedDeltaTime * _speed); 
+            if (direction.sqrMagnitude < SOProjectile.HIT_RANGE * SOProjectile.HIT_RANGE || (directionNormalized * (Time.fixedDeltaTime * _speed)).magnitude > direction.magnitude)
             {
                 // Projectile follow an IDamageable target. Don't need to check if it's null.
                 _attackController.Hit(_target.GetComponent<IDamageable>());
