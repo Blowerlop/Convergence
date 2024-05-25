217e1e125f6aa396ea8bcadafd72d0c00fe50a2e
Hélias <helias.gamonet@laposte.net>
2024-05-25 02:10:40 +0200

    Fix anim event handler serialization ??

--- Unity/Assets/_Project/Scripts/Utilities/AnimationEventHandler.cs
+++ Unity/Assets/_Project/Scripts/Utilities/AnimationEventHandler.cs
@@ -1,14 +1,37 @@
+using System;
 using System.Collections.Generic;
-using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace Project
 {
-    public class AnimationEventHandler : SerializedMonoBehaviour
+    public class AnimationEventHandler : MonoBehaviour
     {
-        [SerializeField] private Dictionary<string, UnityEvent> events  = new();
+        [Serializable]
+        private struct Wrapper
+        {
+            public string id;
+            public UnityEvent unityEvent;
+        }
         
+        [SerializeField] private List<Wrapper> wrappers = new();
+        
+        private Dictionary<string, UnityEvent> _events  = new();
+
+        private void Awake()
+        {
+            foreach (var wrapper in wrappers)
+            {
+                if (_events.ContainsKey(wrapper.id))
+                {
+                    Debug.LogError($"Can't add AnimationEvent {wrapper.id} because it is already registered.");
+                    continue;
+                }
+                
+                _events.Add(wrapper.id, wrapper.unityEvent);
+            }
+        }
+
         // Had to change the name of the method because NetworkAnimator throw an error
         // --> 'Unity Failed to call AnimationEvent Invoke of class NetworkAnimator.
         // The function must have either 0 or 1 parameters and the parameter
@@ -17,13 +40,13 @@ namespace Project
         // Maybe related to https://forum.unity.com/threads/animation-events-problems.723623/
         public void Callback(string id)
         {
-            if (!events.ContainsKey(id))
+            if (!_events.ContainsKey(id))
             {
                 Debug.LogError($"Can't invoke AnimationEvent {id} because it is not registered.");
                 return;
             }
             
-            events[id]?.Invoke();
+            _events[id]?.Invoke();
         }
     }
 }
