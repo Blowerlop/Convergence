150287755a50d436a3ab027108d4c4e8e7c32918
Drainor <60524904+Drainor-GameDev@users.noreply.github.com>
2024-06-06 14:44:11 +0200

    fix transition

--- Unity/Assets/_Project/Scripts/Sound/SimpleSoundPlayer.cs
+++ Unity/Assets/_Project/Scripts/Sound/SimpleSoundPlayer.cs
@@ -10,11 +10,13 @@ namespace Project
     {
         public string eventId, alias;
         public SoundManager.EventType type;
+        public bool playOnStart = true;
         public bool clearOnChange;
         
         void Start()
         {
-            SoundManager.instance.PlayStaticSound(eventId, alias, null, type);
+            if (playOnStart)
+                SoundManager.instance.PlayStaticSound(eventId, alias, null, type);
         }
 
         void Update()
